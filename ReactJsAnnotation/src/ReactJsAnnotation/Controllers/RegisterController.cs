5a785266b0e27f04faf85a485dc6eee6e3236479
phanronald <phanronald@gmail.com>
2016-09-20 22:59:42 -0700

    added register model to use to put into reactjs, modified the tsconfig since it was throwing errors when building

--- ReactJsAnnotation/src/ReactJsAnnotation/Controllers/RegisterController.cs
+++ ReactJsAnnotation/src/ReactJsAnnotation/Controllers/RegisterController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ReactJsAnnotation.Models;
+using ReactJsAnnotation.Code;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,7 +17,54 @@ namespace ReactJsAnnotation.Controllers
 		// GET: /<controller>/
 		public IActionResult Index()
 		{
-			return View();
+			var regModel = new vmRegister()
+			{
+				FirstName = "I AM ME"
+			};
+
+			var displayNames = AttributeExtensions.GetDataAnnotationAttribute<DisplayAttribute>(typeof(vmRegister))
+								.Select(x => x.Name).ToList();
+
+			var type = regModel.GetType();
+			var properties = type.GetProperties();
+			var listOfRegisterInfo = new List<vmRegisterInfo>();
+			for (var i = 0; i < properties.Count(); i++)
+			{
+				var property = properties[i];
+				var dataAnnotationDictionary = new Dictionary<string, List<Attribute>>();
+
+				var displayName = AttributeExtensions.GetAttributeFrom<DisplayAttribute>(regModel, property.Name).Name;
+				var requiredAttr = AttributeExtensions.GetAttributeFrom<RequiredAttribute>(regModel, property.Name);
+				var emailAttr = AttributeExtensions.GetAttributeFrom<EmailAddressAttribute>(regModel, property.Name);
+				var phoneAttr = AttributeExtensions.GetAttributeFrom<PhoneAttribute>(regModel, property.Name);
+				var regexAttr = AttributeExtensions.GetAttributeFrom<RegularExpressionAttribute>(regModel, property.Name);
+				var rangeAttr = AttributeExtensions.GetAttributeFrom<RangeAttribute>(regModel, property.Name);
+				var creditcardAttr = AttributeExtensions.GetAttributeFrom<CreditCardAttribute>(regModel, property.Name);
+
+				var allAttributes = new List<Attribute>()
+				{
+					requiredAttr, emailAttr, phoneAttr, regexAttr, rangeAttr, creditcardAttr
+				}.Where(x => x != null).ToList();
+
+				dataAnnotationDictionary.Add(property.Name, allAttributes);
+
+				var info = new vmRegisterInfo()
+				{
+					FieldName = property.Name,
+					FieldValue = property.GetValue(regModel, null),
+					DisplayName = displayName,
+					DataAnnotations = dataAnnotationDictionary
+				};
+
+				listOfRegisterInfo.Add(info);
+			}
+
+			var model = new vmRegisterContainer()
+			{
+				AllPropertyAnnotation = listOfRegisterInfo
+			};
+
+			return View(model);
 		}
 	}
 }
---
+++ ReactJsAnnotation/src/ReactJsAnnotation/Models/vmRegister.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactJsAnnotation.Models
+{
+	public class vmRegisterContainer
+	{
+		public List<vmRegisterInfo> AllPropertyAnnotation { get; set; }
+	}
+
+	public class vmRegisterInfo
+	{
+		public string FieldName { get; set; }
+		public object FieldValue { get; set; }
+		public string DisplayName { get; set; }
+		public Dictionary<string, List<Attribute>> DataAnnotations { get; set; }
+	}
+
+	public class vmRegister
+	{
+		[Display(Name = "First Name")]
+		[Required(ErrorMessage = "You need first name.")]
+		public string FirstName { get; set; }
+		[Display(Name = "Last Name")]
+		[Required]
+		public string LastName { get; set; }
+		[Display(Name = "Email")]
+		[EmailAddress]
+		public string Email { get; set; }
+		[Display(Name = "Phone")]
+		[Phone]
+		public int Phone { get; set; }
+		[Display(Name = "Address1")]
+		public string Address1 { get; set; }
+		[Display(Name = "Address2")]
+		public string Address2 { get; set; }
+		[Display(Name = "City")]
+		public string City { get; set; }
+		[Display(Name = "State")]
+		public string State { get; set; }
+		[Display(Name = "Zip")]
+		[Required]
+		[RegularExpression(@"^([0-9]{5}(-[0-9]{4})?)$", ErrorMessage = "Please enter exactly 4 numbers.")]
+		public int Zip { get; set; }
+
+		[Display(Name = "Credit Card")]
+		[CreditCard]
+		public int CreditCard { get; set; }
+		[Display(Name = "Number of Employees")]
+		[Range(0, 100)]
+		public int NumberOfEmployees { get; set; }
+
+	}
+}
