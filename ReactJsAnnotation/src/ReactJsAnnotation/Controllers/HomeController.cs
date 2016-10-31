c49043871fa2e20699a67d13cc850db2e4571749
phanronald <phanronald@gmail.com>
2016-10-30 20:52:07 -0700

    added mocha tests for researching

--- ReactJsAnnotation/src/ReactJsAnnotation/Controllers/HomeController.cs
+++ ReactJsAnnotation/src/ReactJsAnnotation/Controllers/HomeController.cs
@@ -6,30 +6,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ReactJsAnnotation.Controllers
 {
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+		public IActionResult About()
+		{
+			ViewData["Message"] = "Your application description page.";
 
-            return View();
-        }
+			return View();
+		}
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+		public IActionResult Contact()
+		{
+			ViewData["Message"] = "Your contact page.";
 
-            return View();
-        }
+			return View();
+		}
 
-        public IActionResult Error()
-        {
-            return View();
-        }
-    }
+		public IActionResult Error()
+		{
+			return View();
+		}
+
+		public IActionResult MochaTest()
+		{
+			return View();
+		}
+	}
 }
---
+++ ReactJsAnnotation/src/ReactJsAnnotation/wwwroot/js/tests/mochatest.js
@@ -0,0 +1,26 @@
+﻿suite('my test', function () {
+
+
+	setup(function () {
+		console.log('setup');
+	});
+
+	teardown(function () {
+		console.log('teardown');
+	});
+
+	test('tests1', function () {
+		expect(1).to.equal(1);
+	});
+
+
+	suite('inner test', function () {
+
+		test('tests2', function () {
+			expect(2).to.equal(2);
+		});
+
+
+	});
+
+});
